b1c7e0fe20ef17850066ed0791f2c3c3aed1b54a
Shekhar-Chaudhary <shekhar.chaudhary@yash.com>
2022-10-06 18:54:27 +0530

    Register and Login

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,32 @@
+import { GoSearch } from "react-icons/go";
+import "./Header.css";
+
 const Header = () => {
   return (
     <>
       <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-              Navbar
+        <nav className="navbar navbar-expand-lg header">
+          <div className="left-div"></div>
+          <div className="container-fluid">
+            <a className="navbar-brand text-white" href="#">
+              <img
+                src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png"
+                width="75px"
+                height="20px"
+              />
             </a>
+
+            <input
+              className=" me-2 search-bar"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+
+            <button className="login-button">Login</button>
+
             <button
-              class="navbar-toggler"
+              className="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#navbarSupportedContent"
@@ -16,77 +34,37 @@ const Header = () => {
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
-              <span class="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">
-                    Home
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
-                    Link
-                  </a>
-                </li>
-                <li class="nav-item dropdown">
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
                   <a
-                    class="nav-link dropdown-toggle"
+                    className="nav-link active ms-5  list-name text-white"
+                    aria-current="page"
                     href="#"
-                    id="navbarDropdown"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
                   >
-                    Dropdown
+                    Become a seller
                   </a>
-                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <hr class="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
                 </li>
-                <li class="nav-item">
-                  <a
-                    class="nav-link disabled"
-                    href="#"
-                    tabindex="-1"
-                    aria-disabled="true"
-                  >
-                    Disabled
+                <li className="nav-item">
+                  <a className="nav-link list-name ms-5 text-white" href="#">
+                    cart
                   </a>
                 </li>
               </ul>
-              <form class="d-flex">
-                <input
-                  class="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-                <button class="btn btn-outline-success" type="submit">
-                  Search
-                </button>
-              </form>
             </div>
           </div>
         </nav>
+
+        <div>
+            <div>
+                <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/9bb3499c8248d2ba.png?q=100" />
+            </div>
+        </div>
       </div>
     </>
   );
